6db95a895dc93a012d23b8bd802e0fb16ec90aaf
wyork507 <wyork507@gmail.com>
2026-07-03 17:38:23 +0800

    Refactor package naming and versioning: update from ASRAD_reader to asrad-reader, change version to 0.4.2, and improve metadata in setup.py, meta.yaml, and README.

--- ASRAD_reader/__init__.py
+++ ASRAD_reader/__init__.py
@@ -4,13 +4,13 @@ This dataset is design for process CWA longtime observe data from https://asrad.
 Created by wyork507. (contact information: https://wyork507.site)
 """
 
-# ASRAD_reader package initialization
+# asrad-reader package initialization
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
-from ASRAD_reader.nan_mode import NanMode
-from ASRAD_reader.obs_data_frame import NanPeriod, ObsDataFrame, ObservePattern
-from ASRAD_reader.loader import load_file, load_folder
+from .nan_mode import NanMode
+from .obs_data_frame import NanPeriod, ObsDataFrame, ObservePattern
+from .loader import load_file, load_folder
 
 __all__ = [
 	"NanMode",
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ASRAD_reader",
-    version='0.4.1',
+    name="asrad-reader",
+    version='0.4.2',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.26.2',
@@ -11,8 +11,8 @@ setup(
     python_requires='>=3.11.7',
     author="wyork507",
     author_email="wyork507@gmail.com",
-    description="A toolkit for processing CWA long-term observation data from https://asrad.pccu.edu.tw.",
-    url="https://wyork507.site",
+    description="A toolkit for processing CWA long-term observation station data from https://asrad.pccu.edu.tw.",
+    url="https://github.com/wyork507/python_asrad_reader",
     long_description=open('readme.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
 )
