062ee87f207abb1b9e12828b812652b479a401e0
Yu Hao Wang <32777871+wyork507@users.noreply.github.com>
2026-07-04 09:56:57 +0800

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-from ASRAD_reader import NanMode
-from ASRAD_reader.loader import load_file
-
-df = load_file(Path("datas/20029999_cwb_hr/20021099.cwb_hr.txt"), mode=NanMode.AllValue)
